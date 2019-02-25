eaf5bb31aed196b22e50c340c07721921239993a
lesterljli <lesterljli@tencent.com>
2019-02-25 13:10:39 +0800

    add
    Signed-off-by: lesterljli <lesterljli@tencent.com>

---
+++ binaryTreeInorderTraversal.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=94 lang=javascript
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (54.85%)
+ * Total Accepted:    405.4K
+ * Total Submissions: 739.2K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,3,2]
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res = [];
+    traversal(root, res);
+    return res
+};
+
+function traversal(root, res) {
+    if (root === null) {
+        return res;
+    }
+
+    traversal(root.left, res);
+    res.push(root.val);
+    traversal(root.right, res);
+}
