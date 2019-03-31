c6e9192175dfb45cda2d3258ab5bff4ec6795ce5
lesterljli <lesterljli@tencent.com>
2019-03-31 21:34:49 +0800

    add
    Signed-off-by: lesterljli <lesterljli@tencent.com>

---
+++ 139.word-break.js
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=139 lang=javascript
+ *
+ * [139] Word Break
+ *
+ * https://leetcode.com/problems/word-break/description/
+ *
+ * algorithms
+ * Medium (34.46%)
+ * Total Accepted:    313K
+ * Total Submissions: 903.6K
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * Given a non-empty string s and a dictionary wordDict containing a list of
+ * non-empty words, determine if s can be segmented into a space-separated
+ * sequence of one or more dictionary words.
+ * 
+ * Note:
+ * 
+ * 
+ * The same word in the dictionary may be reused multiple times in the
+ * segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "leetcode", wordDict = ["leet", "code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet
+ * code".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "applepenapple", wordDict = ["apple", "pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple
+ * pen apple".
+ * Note that you are allowed to reuse a dictionary word.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output: false
+ * 
+ * 
+ */
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+  var set = new Set(wordDict);
+  var dp = [];
+  dp[0] = true;
+  for (var i = 1; i <= s.length; i++) {
+    dp[i] = false;
+  }
+
+  for (var end = 0; end < s.length; end++) {
+    for (var start = 0; start <= end; start++) {
+      if (dp[start] && set.contain(s.substr(start, end-start+1))) {
+        dp[end+1] = true;
+        break;
+      }
+    }
+  }
+  console.log(dp)
+  return dp[dp.length-1];
+};
+
+function Set(arr) {
+  this.set = {}
+  arr.forEach(i => {
+    this.set[i] = i
+  });
+}
+
+Set.prototype.contain = function (v) {
+  if (typeof v === 'string' && this.set[v] !== undefined && this.set[v] !== null) {
+    return true;
+  }
+  return false;
+}
